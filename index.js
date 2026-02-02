278a7ad16934961c670ecbd756d56329ab009227
Albert342545346 <112861843+Albert342545346@users.noreply.github.com>
2026-02-02 22:26:47 +0300

    2 день

--- index.js
+++ index.js
@@ -2,41 +2,82 @@ const hamburgerBtn = document.getElementById('hamburger-btn');
 const navbarMenu = document.getElementById('navbar-menu');
 
 hamburgerBtn.addEventListener('click', () => {
-    // Переключаем активное состояние кнопки
+    if (hamburgerBtn.classList.contains('disable')) {
+        return;
+    }
+    
     hamburgerBtn.classList.toggle('active');
     
-    // Если меню сейчас скрыто (есть класс hidden)
     if (navbarMenu.classList.contains('hidden')) {
-        // Сначала убираем скрытие
+        hamburgerBtn.classList.add('disable');
         navbarMenu.classList.remove('hidden');
-        // Затем добавляем активное состояние (запустится анимация появления)
+
         setTimeout(() => {
             navbarMenu.classList.add('active');
-        }, 10);
+        }, 500);
+
+        setTimeout(() => {
+            hamburgerBtn.classList.remove('disable');
+        }, 1000);
     } else {
-        // Если меню открыто - убираем активное состояние (запустится анимация закрытия)
+        hamburgerBtn.classList.add('disable');
         navbarMenu.classList.remove('active');
-        // Ждём окончания анимации, затем скрываем
         setTimeout(() => {
             navbarMenu.classList.add('hidden');
-        }, 500); // 500ms = длительность transition в CSS
+            hamburgerBtn.classList.remove('disable');
+        }, 1000);
     }
 });
 
 
+// Боковое меню
 const navProfile = document.getElementById('profile');
 const profileSide = document.querySelector('.profile-side');
 
 navProfile.addEventListener('click', () => {
+    if (navProfile.classList.contains('disable')) {
+        return;
+    }
+    
     if (profileSide.classList.contains('hidden')) {
         profileSide.classList.remove('hidden');
+        navProfile.classList.add('disable');
+        
         setTimeout(() => {
             profileSide.classList.add('active');
         }, 10);
+        
+        setTimeout(() => {
+            navProfile.classList.remove('disable');
+        }, 500);
     } else {
         profileSide.classList.remove('active');
+        navProfile.classList.add('disable');
+        
         setTimeout(() => {
             profileSide.classList.add('hidden');
+            navProfile.classList.remove('disable');
         }, 500);
     }
 });
+
+
+// Появляющийся текст
+const textToAnimate = document.getElementById('my-text').textContent;
+const outputElement = document.getElementById('animated-text');
+
+let charIndex = 0;
+const typingSpeed = 30; // мс на символ
+
+function typeWriter() {
+    if (charIndex < textToAnimate.length) {
+        outputElement.textContent += textToAnimate.charAt(charIndex);
+        charIndex++;
+        
+        setTimeout(typeWriter, typingSpeed);
+    }
+}
+
+// Запускаем при загрузке страницы
+window.addEventListener('DOMContentLoaded', typeWriter);
+document.head.appendChild(style);
